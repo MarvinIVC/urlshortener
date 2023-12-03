b3ac88366fbe842969ac270b317ba63f3f1ebbde
hermanboxcar5 <95950881+hermanboxcar5@users.noreply.github.com>
2023-12-03 12:08:46 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,121 @@
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+const QRCode = require('qrcode')
+const express=require('express')
+const app=express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+function nanoid() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+  let result = '';
+
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+async function linkgen(longlink, path){
+  if(!path){
+    path = nanoid()
+  }
+  let bodyops = {
+      domain: 'amb.fr.to',
+      path: 'p/'+path,
+      originalURL: longlink,
+      title: 'public:'+path,
+      tags: ['public']
+    }
+  const url = 'https://api.short.io/links';
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: process.env['key']
+    },
+    body: JSON.stringify(bodyops)
+  };
+  
+  let data = await fetch(url, options)
+  data = await data.json()
+
+  if(data.success==false){
+    if(data.error=='Link already exists'){
+      return {success:false, dup:true}
+    }
+    else {
+      return {success:false, dup:false, error:data.error}
+    }
+  }
+  return {success:true, url:data.shortURL, id:data.idString}
+}
+  const genqr = async text => {
+    try {
+      let responce = await QRCode.toDataURL(text)
+      console.log(responce)
+      return responce
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+// link('https://google.com/hello', 'hello').then(a=>{console.log(a)})
+
+
+app.get('/', (req, res) => {
+  res.send(`
+  <form action="/" method="post">
+    <label for="longLink">Long Link:</label>
+    <input type="text" id="longLink" name="longLink" required><br>
+    <label for="customBackHalf">Custom Back Half (optional):</label>
+    <input type="text" id="customBackHalf" name="customBackHalf"><br>
+    <label for="password">Generate QR Code:</label>
+    <input type="checkbox" id="qr" name="qr"><br>
+    <input type="submit" value="Create Short Link">
+  </form>
+  `)
+});
+
+app.post('/', async (req, res) => {
+  let link = req.body.longLink
+  if(!link.startsWith('http://') && !link.startsWith('https://')){
+    link = 'https://'+link
+  }
+  let path = req.body.customBackHalf
+  let qr = req.body.qr
+  let data = await linkgen(link, path)
+  if(data.success==false){
+    if(data.dup==true){
+      res.send(`<h1>Link already exists</h1> 
+      <a href='/'> <-- Back to home</a>`)
+    } else {
+      res.send(`<h1>An error occured</h1> 
+      <p>ERROR: ${data.error}</p>
+      <p>Please screenshot this page and sent it to devs to resolve issue</p>
+      <a href='/'> <-- Back to home</a>`)
+    }
+  } else {
+    console.log(qr)
+    if(qr){
+      
+      qr = await genqr(data.url)
+      qr = "QR code: "+`<img src='${qr}' alt = 'qrcode'>`
+    } else {
+      qr = ''
+    }
+    console.log(qr)
+    console.log(data)
+    res.send(`
+    <h1>Link Created</h1>
+    <p>Short Link: <a href='${data.url}'>${data.url}</a></p>
+    <p>${qr}</p>
+    <a href='/'> <-- Back to home</a>
+    `)
+  }
+})
+
+app.listen(3000, () => {
+  console.log('server started');
+});
